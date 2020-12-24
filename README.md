db78d639a886a740dbf7e9ec218795de0c253cec
Thomas Wang <thomas@teaminkling.com>
2020-12-24 20:46:33 +1100

    Get NPM run commands to work

--- README.md
+++ README.md
@@ -7,12 +7,14 @@ This is the original tagline:
 > A unique tag-based theme to arrange your publications into collections. Keep organized and let your readers explore
 > your publications with ease. Completely free and fully responsive, released under the MIT license.
 
-# Installation
+## Installation
+
+Make sure you're using a production-ready version of Node (LTS).
 
 1. Download the repository as a `.zip` file.
 2. Log into Ghost, and go to the `Design` settings area to upload the zip file.
 
-# Development
+## Development
 
 Styles are compiled using Gulp/PostCSS to polyfill future CSS spec. You'll need [Node](https://nodejs.org/),
 [Yarn](https://yarnpkg.com/) (or NPM) and [Gulp](https://gulpjs.com) installed globally. After that, from the theme's 
@@ -28,14 +30,25 @@ Now you can edit `/assets/css/` files, which will be compiled to `/assets/built/
 The `zip` Gulp task packages the theme files into `dist/<theme-name>.zip`, which you can then upload to your site.
 
 ```bash
-yarn zip
+npm run zip
 ```
 
-# PostCSS Features Used
+## Automatic Deployment
+
+Go into your repository settings and set:
+
+```yaml
+api-url: ${{ secrets.GHOST_ADMIN_API_URL }}
+api-key: ${{ secrets.GHOST_ADMIN_API_KEY }}
+```
+
+You will get a new deployment every time you tag and push `v<semver>` e.g., `v1.2.3`.
+
+## PostCSS Features Used
 
 - **Autoprefixer**: Don't worry about writing browser prefixes of any kind, it's all done automatically with support for
   the latest 2 major versions of every browser.
 
-# Copyright & License
+## Copyright & License
 
 Copyright (c) 2013-2020 Ghost Foundation - Released under the [MIT license](LICENSE).
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
+/*jshint esversion: 6, node: true */
+
+"use strict";
+
+
 const {series, parallel, watch, src, dest} = require('gulp');
 const pump = require('pump');
+
 
 // gulp plugins and utils
 const livereload = require('gulp-livereload');
@@ -8,16 +14,20 @@ const postcss = require('gulp-postcss');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const beeper = require('beeper');
+const zip = require('gulp-zip');
+
 
 // postcss plugins
 const easyimport = require('postcss-easy-import');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
+
 function serve(done) {
     livereload.listen();
     done();
 }
+
 
 function handleError(done) {
     return function (err) {
@@ -26,7 +36,8 @@ function handleError(done) {
         }
         return done(err);
     };
-};
+}
+
 
 function hbs(done) {
     pump([
@@ -34,6 +45,7 @@ function hbs(done) {
         livereload()
     ], handleError(done));
 }
+
 
 function css(done) {
     pump([
@@ -48,6 +60,7 @@ function css(done) {
     ], handleError(done));
 }
 
+
 function js(done) {
     pump([
         src([
@@ -61,6 +74,7 @@ function js(done) {
     ], handleError(done));
 }
 
+
 function lint(done) {
     pump([
         src(['assets/css/**/*.css', '!assets/css/vendor/*']),
@@ -73,6 +87,7 @@ function lint(done) {
         dest('assets/css/')
     ], handleError(done));
 }
+
 
 function zipper(done) {
     const filename = require('./package.json').name + '.zip';
@@ -89,11 +104,14 @@ function zipper(done) {
     ], handleError(done));
 }
 
+
+
 const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs'], hbs);
 const cssWatcher = () => watch('assets/css/**/*.css', css);
 const jsWatcher = () => watch('assets/js/**/*.js', js);
 const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher);
 const build = series(css, js);
+
 
 exports.build = build;
 exports.lint = lint;
